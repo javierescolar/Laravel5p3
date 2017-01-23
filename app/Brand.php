5f14c87f5cfe961c4323761e4190fbc1d0536396
javierescolar <javierescolar10@gmail.com>
2017-01-23 18:38:52 +0100

    desarrollo de tag new para marcas que tengan menos de dos días

--- app/Brand.php
+++ app/Brand.php
@@ -3,17 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTime;
 
-class Brand extends Model
-{
-    
+class Brand extends Model {
+
     protected $table = 'brands';
-    protected $fillable = ['name','slug','logo'];
-    protected $hidden = ['created_at','updated_at'];
-    
-     
-    public function products()
-    {
-    	return $this->hasMany('App\Product');
+    protected $fillable = ['name', 'slug', 'logo'];
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function products() {
+        return $this->hasMany('App\Product');
     }
+
+    public function calculateDaysCreated() {
+        $date1 = new DateTime($this->attributes['created_at']);
+        $date2 = new DateTime(date('Y-m-d h:i:s'));
+        $interval = $date1->diff($date2);
+        return $interval->format('%a');
+    }
+
 }
