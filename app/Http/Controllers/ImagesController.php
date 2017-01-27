ed3900a7264244078e9bfb4b4181123283acaae2
javierescolar <javierescolar10@gmail.com>
2017-01-27 16:52:54 +0100

    solucion imagens offer y producto añadido

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -15,7 +15,14 @@ class ImagesController extends Controller {
     
 
     public function index(Brand $brand, Product $product) {
-        $images = $product->images()->get();
+        //$images = $product->images()->get();
+        $imagesAll = $product->images()->get();
+        $images = [];
+        foreach ($imagesAll as $img) {
+            if ($img->offer != 1) {
+                array_push($images, $img);
+            }
+        }
         return view('images.index', compact('product', 'images'));
     }
 }
